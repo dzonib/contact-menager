b58f75286bac4d327b6987392575b85ef9a1f297
dzonib <nikolabosnjak381@gmail.com>
2018-07-26 20:33:24 +0200

    forms next

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 import React, { Component } from 'react';
-import Contact from './Components/Contact';
 import Header from './Components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Contacts from './Components/Contacts';
+import {Provider} from './context'
 
 export default class App extends Component {
 
-  state = {
-    contacts: [
-      {id: 1, name: "John Doe", email: "johndoe@gmail.com", phone: "225993"},
-      {id: 2, name: "Kong", email: "kingkong@gmail.com", phone: "568566369"},
-      {id: 3, name: "King", email: "kingkong@gmail.com", phone: "5898745454"},
-    ]
-  }
-
-  removeItem = id => {
-    let updatedContacts = this.state.contacts.filter(item => item.id !== id)
-    console.log(updatedContacts)
-    this.setState(() => ({
-      contacts: updatedContacts
-    }))
-  }
-
   render() {
-    const {contacts} = this.state;
-
     return (
-      <div className="App">
-        <Header branding="Contact Menager"/>
-        <div className="container">
-          {contacts.map( contact => <Contact
-          removeItem={this.removeItem}
-          key={contact.id} 
-          {...contact}
-           />)}
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Menager"/>
+          <Contacts />
         </div>
-      </div>
+      </Provider>
     );
   }
 }
---
+++ src/context.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.filter(({ id }) => id !== action.payload)
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      { id: 1, name: 'John Doe', email: 'johndoe@gmail.com', phone: '225993' },
+      { id: 2, name: 'Kong', email: 'kingkong@gmail.com', phone: '568566369' },
+      { id: 3, name: 'King', email: 'kingkong@gmail.com', phone: '5898745454' }
+    ],
+    dispatch: action => this.setState(state => reducer(state, action))
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
